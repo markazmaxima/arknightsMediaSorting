e3c64521044d1e0f165a86ec376877748839b1d2
Markaz <markaz7821@gmail.com>
2022-02-15 17:08:11 +0800

    update vigilo pv
    and some experimental html

---
+++ js/musicPlayer.js
@@ -0,0 +1,6 @@
+<link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'><link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>// JavaScript Document
+
+
+
+
+

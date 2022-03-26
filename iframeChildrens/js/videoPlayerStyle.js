6841e9d193dfc563845e668be28c0b7dbf53a9cd
Markaz <markaz7821@gmail.com>
2022-03-26 11:34:38 +0800

    new Site News section
    more to come...

---
+++ iframeChildrens/js/videoPlayerStyle.js
@@ -0,0 +1,92 @@
+function ep01(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/6C5sfOOvWwA?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - Code of Brawl [Speed of Light] DJ OKAWARI feat. Ai Ninomiya";
+}
+function ep02(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/Q_Fs6G77e_M?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Boiling Blood]";
+}
+function ep03(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/3NG4ThzuC-0?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Timeless]";
+}
+function ep04(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/DId_qx80PHY?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Solitary Journey]";
+}
+function ep05(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/WVzijZXuT3o?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Hometown Wind]";
+}
+function ep06(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/o-pH0UIf3kE?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Gazing From Great Heights]";
+}
+function ep07(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/u2h6igBRuOc?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Lily of the Valley]";
+}
+function ep08(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/kaOcSI6JaZ0?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Zone 10⁻⁸]";
+}
+function ep09(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/SBCk2PRiZ1w?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Curtain Call]";
+}
+function ep10(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/rJuSmn9tovI?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Renegade]";
+}
+function ep11(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/WTZ5VSmPU9Q?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Requiem]";
+}
+function ep12(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/YUAq2wcZybg?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Sparkling Hydraulics]";
+}
+function ep13(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/JoFzeb-bbxc?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [El Brillo Solitario]";
+}
+function ep14(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/whAo2g0_6DY?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Evolutionary Mechanization]";
+}
+function ep15(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/mnS5JqFojUw?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [ALIVE]";
+}
+function ep16(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/KIh8PEwFCtg?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Spring's Pulse]";
+}
+function ep17(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/orfsrwAhNvc?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Reconnection]";
+}
+function ep18(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/tk3HjygbXA8?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Stay Gold]";
+}
+function ep19(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/bu0x7pMiMIw?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Confront]";
+}
+function ep20(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/2b1BiBSLCqg?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Arknights EP - [Crystallize]";
+}
+function ep21(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function ep22(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function ep(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}

1dbc81df2cbd4e3326d5da685658148f3bcec402
Markaz <markaz7821@gmail.com>
2022-09-12 11:45:44 +0800

    0.6.2 (12 September 2022)

---
+++ video.events/js/eventJS01.js
@@ -0,0 +1,40 @@
+function event01(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/PCkOtRDvtjw?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Grani and the Knights’ Treasure";
+}
+function event02(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/lLSaBSdbKZ8?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Operational Intelligence";
+}
+function event03(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/soODvXnyR7g?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "Heart of Surging Flame";
+}
+function event04(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event05(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event06(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event07(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event08(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event09(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}
+function event10(){
+	parent.document.getElementById("primary-video-wrapper").src = "https://www.youtube.com/embed/?autoplay=1";
+	parent.document.getElementById("player-title").innerHTML = "";
+}

dd053275a3553ddf9c8f7c188cedb43b0e2f9898
Markaz <markaz7821@gmail.com>
2022-09-15 13:26:30 +0800

    0.6.30 (15 September 2022)
    finally able to duplicate element, set attribute of multiple parent and child element and able to pull data from json then assign it in order.
    
    further testing is on the way...

--- js/exp.jsonMod.js
+++ js/exp.jsonMod.js
@@ -1,39 +1,49 @@
 "use strict";
 import master from '../json/test.json' assert {type: 'json'};
 
-
-
-let chl = document.getElementById("btn-list").childNodes;
-console.log(chl);
-
 $(document).ready(function () {
+    var i = 0;
+    var mxD = 10; //change here for duplication amount !!!
+
     dupl();
-    var i = 1;
-    var mxD = 10;
 
-    let titl1 = master[i=i-1].tit;
-
-    function dupl() {
+    function dupl() { //loop duplicate
         const myTime = setTimeout(() => {
-            if (i == mxD) { //last dupl check
-                $("#btn-list-item:first-child").attr("onclick", "vid1()");
-                //pull data for first 3 duplicate
+            let grabTitle = master[i].tit; //grab data
+            console.log(grabTitle);
+            i++;
 
-                console.log("pull "+titl1);
-                //put data in first 3 btn
+            if (i == mxD) { //last dupl check
+                $("#btn-list-item").attr("onclick", "vid1()");
+                $("#btn-list-item:first-child").attr("id", "btn=list-item-1");
+                $("#title-1").text("null1");
+                $("#title-2").text("null2");
+
+                console.log("end of process");
             }
             else if (i < mxD) { //dupl function
-                i++;
                 $("#btn-list-item").attr("onclick", "vid" + i + "()");
-                $("#btn-list-item").clone().appendTo("#btn-list");
+                $("#btn-list-item").clone().appendTo("#btn-list"); //dupl based from first child
+                let getchildList = document.getElementById("btn-list").childNodes[i]; //get child of list
+                let getchildBtn = getchildList.childNodes[1]; //get child of button
 
-                dupl();
+                //set new btn id
+                var nT = i + 1;
+                let nwbtnID = "btn-list-item-" + nT;
+                getchildList.id = nwbtnID;
+
+                //set new title id
+                let nwitmID = "title-" + i;
+                getchildBtn.id = nwitmID;
+
+                //set new title by id
+                const tempTkn = document.getElementById("title-"+i);
+                tempTkn.innerHTML = grabTitle;
+
+                dupl(); //loop
             } else {
                 console.log("loop broken...");
             }
-        }, 100);
+        }, 300);
     };
-
-    let ppp = $("#btn-list-item");
-    console.log(ppp);
 });

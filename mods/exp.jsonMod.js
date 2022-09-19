74083fafea825662afed3f9b73e00881e8bec4af
Markaz <markaz7821@gmail.com>
2022-09-19 11:55:42 +0800

    0.6.40 (19 September 2022)
    finally able to fetch URL from json file and add it in button tru loop...

--- mods/exp.jsonMod.js
+++ mods/exp.jsonMod.js
@@ -1,5 +1,5 @@
 "use strict";
-import master from '../json/test.json' assert {type: 'json'};
+import master from '../json/list.cncpt.01.json' assert {type: 'json'};
 
 $(document).ready(function () {
     var i = 0;
@@ -9,18 +9,20 @@ $(document).ready(function () {
 
     function dupl() { //loop duplicate
         const myTime = setTimeout(() => {
-            let grabTitle = master[i].tit; //grab data
+            let grabTitle = master[i].title; //grab data
+            let grabURL = master[i].URL; //grab URL
             let grabTblock = master.length;
-            console.log(grabTitle);
+            console.log(grabURL);
             i++;
 
-            if (i == mxD) { //last dupl check
+            if ((i + 1) == mxD) { //last dupl check
+                $("#btn-list-item:first-child").attr("value", "1");
                 
             }
             else if (i < mxD) { //dupl function
-                $("#btn-list-item").attr("onclick", "vid" + i + "()");
                 $("#btn-list-item").clone().appendTo("#btn-list"); //dupl based from first child
                 let getchildList = document.getElementById("btn-list").childNodes[i]; //get child of list
+                $("#btn-list-item:last-child").attr("value", (i+1)); //set duplicated btn value
                 let getchildBtn = getchildList.childNodes[1]; //get child of button
 
                 //set new btn id
@@ -35,16 +37,22 @@ $(document).ready(function () {
                 if (i == grabTblock) {
                     const remC = document.getElementById("btn-list");
                     remC.removeChild(remC.lastElementChild);
+
+                    $("#btn-list-item:first-child").attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = 'https://www.youtube.com/embed/Hfu_I0V3VrM?autoplay=1';");
                 };
 
                 //set new title by id
                 const tempTkn = document.getElementById("title-" + (i - 1));
                 tempTkn.innerHTML = grabTitle;
 
+                //set onclick
+                const tempInt = document.getElementById("btn-list-item-" + (i));
+                $(tempInt).attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = '"+grabURL+"';");
+
                 dupl(); //loop
             } else {
                 console.log("loop broken..."); //error output
             }
-        }, 500); //loop delayer
+        }, 200); //loop delayer
     };
 });

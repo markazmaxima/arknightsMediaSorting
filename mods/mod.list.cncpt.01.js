4e64008959f912ec35020bd82b099fa102c2fac8
Markaz <markaz7821@gmail.com>
2022-09-19 15:16:29 +0800

    0.7.00 (19 September 2022)
    new and improve file efficiency.
    introducing "dynamic playlist".

--- mods/mod.list.cncpt.01.js
+++ mods/mod.list.cncpt.01.js
@@ -1,9 +1,8 @@
 "use strict";
-import master from '../json/list.main.01.json' assert {type: 'json'};
+import master from '../json/list.cncpt.01.json' assert {type: 'json'};
 
 $(document).ready(function () {
     var i = 0;
-    var mxD = 20; //change here for duplication amount !!!
 
     dupl();
 
@@ -11,15 +10,13 @@ $(document).ready(function () {
         const myTime = setTimeout(() => {
             let grabTitle = master[i].title; //grab data
             let grabURL = master[i].URL; //grab URL
+            let grabTag = master[i].tag; //grab tag
+            let grabDur = master[i].duration; //grab duration
             let grabTblock = master.length;
             console.log(grabURL);
             i++;
 
-            if ((i + 1) == mxD) { //last dupl check
-                $("#btn-list-item:first-child").attr("value", "1");
-                
-            }
-            else if (i < mxD) { //dupl function
+            if (i < grabTblock+1) { //dupl function
                 $("#btn-list-item").clone().appendTo("#btn-list"); //dupl based from first child
                 let getchildList = document.getElementById("btn-list").childNodes[i]; //get child of list
                 $("#btn-list-item:last-child").attr("value", (i+1)); //set duplicated btn value
@@ -34,11 +31,10 @@ $(document).ready(function () {
                 let nwitmID = "title-" + i;
                 getchildBtn.id = nwitmID;
 
+                //last duplicate error interf
                 if (i == grabTblock) {
                     const remC = document.getElementById("btn-list");
                     remC.removeChild(remC.lastElementChild);
-
-                    $("#btn-list-item:first-child").attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = 'https://www.youtube.com/embed/Hfu_I0V3VrM?autoplay=1';");
                 };
 
                 //set new title by id
@@ -47,12 +43,12 @@ $(document).ready(function () {
 
                 //set onclick
                 const tempInt = document.getElementById("btn-list-item-" + (i));
-                $(tempInt).attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = '"+grabURL+"';");
+                $(tempInt).attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = '"+grabURL+"';parent.document.getElementById('player-title').innerHTML='"+grabTitle+"'");
 
                 dupl(); //loop
             } else {
                 console.log("loop broken..."); //error output
             }
-        }, 200); //loop delayer
+        }, 0); //loop delayer
     };
 });
---
+++ mods/mod.list.extd.02.js
@@ -0,0 +1,54 @@
+"use strict";
+import master from '../json/list.extd.02.json' assert {type: 'json'};
+
+$(document).ready(function () {
+    var i = 0;
+
+    dupl();
+
+    function dupl() { //loop duplicate
+        const myTime = setTimeout(() => {
+            let grabTitle = master[i].title; //grab data
+            let grabURL = master[i].URL; //grab URL
+            let grabTag = master[i].tag; //grab tag
+            let grabDur = master[i].duration; //grab duration
+            let grabTblock = master.length;
+            console.log(grabURL);
+            i++;
+
+            if (i < grabTblock+1) { //dupl function
+                $("#btn-list-item").clone().appendTo("#btn-list"); //dupl based from first child
+                let getchildList = document.getElementById("btn-list").childNodes[i]; //get child of list
+                $("#btn-list-item:last-child").attr("value", (i+1)); //set duplicated btn value
+                let getchildBtn = getchildList.childNodes[1]; //get child of button
+
+                //set new btn id
+                var nT = i + 1;
+                let nwbtnID = "btn-list-item-" + nT;
+                getchildList.id = nwbtnID;
+
+                //set new title id
+                let nwitmID = "title-" + i;
+                getchildBtn.id = nwitmID;
+
+                //last duplicate error interf
+                if (i == grabTblock) {
+                    const remC = document.getElementById("btn-list");
+                    remC.removeChild(remC.lastElementChild);
+                };
+
+                //set new title by id
+                const tempTkn = document.getElementById("title-" + (i - 1));
+                tempTkn.innerHTML = grabTitle;
+
+                //set onclick
+                const tempInt = document.getElementById("btn-list-item-" + (i));
+                $(tempInt).attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = '"+grabURL+"';parent.document.getElementById('player-title').innerHTML='"+grabTitle+"'");
+
+                dupl(); //loop
+            } else {
+                console.log("loop broken..."); //error output
+            }
+        }, 0); //loop delayer
+    };
+});

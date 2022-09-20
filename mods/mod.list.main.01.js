08a8257322c4cecae461b2cfff89cb980a61b168
Markaz <markaz7821@gmail.com>
2022-09-20 11:04:19 +0800

    0.8.0 (20 September 2022)
    tidy up the playlist modules.

--- mods/mod.list.main.01.js
+++ mods/mod.list.main.01.js
@@ -4,56 +4,34 @@ import master from '../json/list.main.01.json' assert {type: 'json'};
 $(document).ready(function () {
     var i = 0;
 
-    dupl();
+    setData();
 
-    function dupl() { //loop duplicate
-        const myTime = setTimeout(() => {
-            let grabTitle = master[i].title; //grab data
-            let grabURL = master[i].URL; //grab URL
-            let grabTag = master[i].tag; //grab tag
-            let grabDur = master[i+1].duration; //grab duration
-            let grabTblock = master.length;
-            console.log(grabURL);
+    function setData() {
+        let grabTitle = master[i].title; //grab data
+        let grabURL = master[i].URL; //grab URL
+        let grabTag = master[i].tag; //grab tag
+        let grabDur = master[i].duration; //grab duration
+        let grabArrLngth = master.length; //grab array length
+        grabArrLngth = grabArrLngth - 1;
+
+        let getBtnLst = document.getElementById("btn-lst").childNodes[i]; //fetch btn nodes
+        let getInBtn = getBtnLst.childNodes[1]; //seek btn, title
+        let getInfDur = getBtnLst.childNodes[2]; //seek btn, duration
+
+        $(getBtnLst).attr("onclick", "parent.document.getElementById('prmry-vid-wrppr').src = 'https://www.youtube.com/embed/"+grabURL+"?autoplay=1';parent.document.getElementById('player-title').innerHTML='"+grabTitle+"'");
+
+        getInBtn.innerHTML = grabTitle;
+        getInfDur.innerHTML = grabDur;
+        if (i < grabArrLngth) {
+            duplicate();
+        };
+    };
+    function duplicate() {
+        const loop = setTimeout(() => {
+            $("#btnLst-itm").clone().appendTo("#btn-lst");
             i++;
-
-            if (i < grabTblock+1) { //dupl function
-                $("#btn-list-item").clone().appendTo("#btn-list"); //dupl based from first child
-                let getchildList = document.getElementById("btn-list").childNodes[i]; //get child of list
-                $("#btn-list-item:last-child").attr("value", (i+1)); //set duplicated btn value
-                let getchildBtn = getchildList.childNodes[1]; //get child of button
-                let getchild2Btn = getchildList.childNodes[3];
-                let getchild3Btn = getchild2Btn.childNodes[1];
-
-                //set new btn id
-                var nT = i + 1;
-                let nwbtnID = "btn-list-item-" + nT;
-                getchildList.id = nwbtnID;
-
-                //set new title id
-                let nwitmID = "title-" + i;
-                getchildBtn.id = nwitmID;
-
-                //set new duration
-                let nwitmDur = grabDur;
-                getchild3Btn.innerHTML = nwitmDur;
-
-                if (i == grabTblock) {
-                    const remC = document.getElementById("btn-list");
-                    remC.removeChild(remC.lastElementChild);
-                };
-
-                //set new title by id
-                const tempTkn = document.getElementById("title-" + (i - 1));
-                tempTkn.innerHTML = grabTitle;
-
-                //set onclick
-                const tempInt = document.getElementById("btn-list-item-" + (i));
-                $(tempInt).attr("onclick", "parent.document.getElementById('primary-video-wrapper').src = '"+grabURL+"';parent.document.getElementById('player-title').innerHTML='"+grabTitle+"'");
-
-                dupl(); //loop
-            } else {
-                console.log("loop broken..."); //error output
-            }
-        }, 0); //loop delayer
+            console.log(i+" duplicated");
+            setData();
+        }, 100);
     };
 });

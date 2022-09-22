6126d6ec5db3991f7dc45ee73bcb2d2cf55eeb48
Markaz <markaz7821@gmail.com>
2022-09-22 16:36:15 +0800

    0.8.41 (22 September 2022)

--- js/newMsc.plyr.js
+++ js/newMsc.plyr.js
@@ -3,54 +3,103 @@ $(document).ready(function () {
     var mscPlyr;
     var plyBtn;
     var pauBtn;
-    var txtCurrSec;
-    var txtCurrMin
-    var maxSldr;
+    var loopBtn;
+    var muteBtn;
+    var fullDur;
+    var liveDur;
+    var volSldr;
+    var durSldr;
+    var volTxt;
     var maxTime;
-    var txtMaxDur;
     var secCount;
     var minCount;
     initial();
-    function initial() {
+
+    function initial() { //initial data grabbing
         mscPlyr = document.getElementById("player");
+        //buttons
         plyBtn = document.getElementById("play");
         pauBtn = document.getElementById("pause");
-        maxSldr = document.getElementById("dur-sldr"); //obj
-        txtCurrSec = document.getElementById("currSec"); //obj
-        txtCurrMin = document.getElementById("currMin"); //obj
-        txtMaxDur = document.getElementById("maxDur");
-        maxTime = mscPlyr.duration; //grab max duration
-    }
-    mscPlyr.ontimeupdate = function() {trackDur()};
+        loopBtn = document.getElementById("loop");
+        muteBtn = document.getElementById("mute");
+        //obj value
+        volSldr = document.getElementById("vol-sldr");
+        durSldr = document.getElementById("dur-sldr");
+        //string
+        fullDur = document.getElementById("fullDur");
+        liveDur = document.getElementById("liveDur");
+        volTxt = document.getElementById("vol-txt");
+    };
+    mscPlyr.ontimeupdate = function () { trackDur() }; //show music prog in bar
     function trackDur() {
         var grabCurrDur = mscPlyr.currentTime; //grab current duration
         tknCurSec = Math.trunc(grabCurrDur); //set second
-        tknCurMin = Math.trunc(grabCurrDur/60); //set minute
-
-        minusSec = (tknCurMin*60);
+        tknCurMin = Math.trunc(grabCurrDur / 60); //set minute
 
         minCount = tknCurMin;
-        secCount = (tknCurSec-minusSec);
+        minusSec = (tknCurMin * 60);
+        secCount = (tknCurSec - minusSec); //set every second and reset count every 60 sec
 
-        txtCurrSec.innerHTML = secCount;
-        txtCurrMin.innerHTML = tknCurMin;
-    }
+        if (secCount < 10) {
+            secCount = "0" + secCount;
+        }
+        liveDur.innerHTML = (tknCurMin + ":" + secCount);
+
+        durSldr.value = tknCurSec;
+    };
+    $("#vol-sldr").on("input", function(){ //set player volume
+        var grabVol = volSldr.value;
+        tempVol = grabVol/100;
+        mscPlyr.volume = tempVol;
+        volTxt.innerHTML = grabVol;
+    });
     $("#play").click(function () {
         mscPlyr.play();
-        txtMaxDur.innerHTML = Math.trunc(maxTime);
+        maxTime = mscPlyr.duration; //grab max duration
+        var tknMaxDur = Math.trunc(maxTime);
+        durSldr.max = tknMaxDur; //set bar max
+
+        //full music duration
+        fullMin = Math.round(maxTime / 60);
+        fullSec = Math.round(maxTime / 60);
+        if (fullSec < 10) {
+            fullSec = "0" + fullSec;
+        }
+        fullDur.innerHTML = (fullMin + ":" + fullSec);
 
         plyBtn.hidden = true;
         pauBtn.hidden = false;
         plyBtn.value = "0";
         pauBtn.value = "1";
-    })
-    $("#pause").click(function () {
+    });
+    $("#pause").click(function () { //pause player
         mscPlyr.pause();
 
         plyBtn.hidden = false;
         pauBtn.hidden = true;
         plyBtn.value = "1";
         pauBtn.value = "0";
-    })
-})
-
+    });
+    $("#loop").click(function () { //loop player
+        var tknV = loopBtn.value;
+        if (tknV == 0) {
+            loopBtn.value = "1";
+            mscPlyr.loop = true;
+        }
+        if (tknV == 1) {
+            loopBtn.value = "0";
+            mscPlyr.loop = false;
+        }
+    });
+    $("#mute").click(function () { //mute player
+        var tknV = muteBtn.value;
+        if (tknV == 0) {
+            muteBtn.value = "1";
+            mscPlyr.muted = true;
+        }
+        if (tknV == 1) {
+            muteBtn.value = "0";
+            mscPlyr.muted = false;
+        }
+    });
+});
